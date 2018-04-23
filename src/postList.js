c5643162eb6a766c72cc5e2a242ae73d3f42d8ac
Terry Patterson <10890854+TerryRPatterson@users.noreply.github.com>
2018-04-23 14:54:09 -0400

    Finished Redux

--- src/postList.js
+++ src/postList.js
@@ -1,52 +1,44 @@
-import React, {Component} from "react";
+import React from "react";
 import post from "./post";
 import postCreator from "./postCreator";
 import loading from "./loading";
-import deepClone from "lodash.clonedeep";
 import "./postList.css";
+import {connect} from "react-redux";
 
 let createElement = React.createElement;
+let mapStateToProps = (state) => {
+    let newProps = {};
+    newProps["user"] = state["user"];
+    newProps["postList"] = state["posts"];
+    return newProps;
+};
+let postList =  ({postList,match,user}) => {
+    let editable = true;//Change to canPost
+    let postCreatorHolder;
+    let vDomList;
+    if (match){
+        if(match["params"]["username"] === user){
+            editable = true;
+        }
+    }
+    if (postList && Object.keys(postList).length > 0) {
+        vDomList = Object.keys(postList).map((id) => {
+            let postInfo = postList[id];
+            return createElement(post,Object.assign(postInfo,{id, key:id}));
+        });
+    } else {
+        vDomList = createElement(loading,{key:"loading"});
+    }
+    if (editable){
+        postCreatorHolder = createElement(postCreator,{key:"postCreator",
+            user:user});
+    }
+    let postDisplay = createElement("div",{className:
+        postList && Object.keys(postList).length > 0 ? "postList" : "postList loading"
+    },vDomList);
+    return createElement("div",{className:"postDisplay"}, postDisplay,
+        postCreatorHolder);
+};
 
-class postList extends Component{
-    constructor({state, props, match,user}){
-        super(props);
-        this.state = state || {postList:[]};
-        this.state.editable = true;
-        this.state.user = user;
-        if (match){
-            if(match["params"]["username"] === user){
-                this.state["editable"] = true;
-            }
-        }
-    }
-    render (){
-        let list = this.state["postList"];
-        let postCreatorHolder;
-        let vDomList;
-        if (list && list.length > 0) {
-            vDomList = list.map((postInfo) => {
-                let {id} = postInfo;
-                return createElement(post,Object.assign(postInfo,{id, key:id}));
-            });
-        } else {
-            vDomList = createElement(loading,{key:"loading"});
-        }
-        if (this.state.editable){
-            postCreatorHolder = createElement(postCreator,{key:"postCreator",
-                updatePostList:this.updatePostList.bind(this),
-                user:this.state.user});
-        }
-        let postDisplay = createElement("div",{className:
-            list && list.length > 0 ? "postList" : "postList loading"
-        },vDomList);
-        return createElement("div",{className:"postDisplay"}, postDisplay,
-            postCreatorHolder);
-    }
-    updatePostList(newPost){
-        let newList = deepClone(this.state.postList);
-        newList.push(newPost);
-        this.setState({postList:newList});
-    }
-}
-
-export default postList;
+let connectedPostList = connect(mapStateToProps)(postList);
+export default connectedPostList;
